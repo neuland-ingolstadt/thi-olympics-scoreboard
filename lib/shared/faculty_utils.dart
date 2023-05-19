efdf17e1af7b44f845c8bb7421f91e1f2c7c5477
Philipp Opheys <philipp@opheys.dev>
2023-05-19 22:35:35 +0200

    ⚡️hardcoded icon data to use font reduction

--- lib/shared/faculty_utils.dart
+++ lib/shared/faculty_utils.dart
@@ -4,7 +4,26 @@ import 'package:scoreboard/services/hex_color.dart';
 
 class FacultyUtils {
   static IconData iconFromFaculty(BuildContext context, Faculty faculty) {
-    return IconData(faculty.icon, fontFamily: 'MaterialIcons');
+    switch (faculty.icon) {
+      case 63006:
+        return Icons.cases_rounded;
+      case 57896:
+        return Icons.electrical_services_rounded;
+      case 983134:
+        return Icons.people_rounded;
+      case 58215:
+        return Icons.laptop_rounded;
+      case 983396:
+        return Icons.settings_rounded;
+      case 983842:
+        return Icons.forest_rounded;
+      case 58091:
+        return Icons.group_rounded;
+      case 63077:
+        return Icons.construction_rounded;
+      default:
+        return Icons.error_rounded;
+    }
   }
 
   static CircleAvatar getCircleAvatar(BuildContext context, Faculty faculty) {
